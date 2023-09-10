6bc43adbaa6823ab18bcbac80ed364d3016729a2
Bharat Agrawal <141904232+bharatagrawal12004@users.noreply.github.com>
2023-09-10 09:24:17 +0530

    little q4 left and q3 left bonus 1 left

--- Assignments/Assignment_4/q4_2Dlists.py
+++ Assignments/Assignment_4/q4_2Dlists.py
@@ -15,8 +15,36 @@ rows=int(input("Enter the number of rows: "))
 cols=int(input("Enter the number of columns: "))
 l=(create_2d_matrix(rows,cols))    #this creates a 2d list and stores it in a variable l
 
-#this prints the 2d list in matrix format
-for i in l:
+
+for i in l:             #this prints the 2d list in matrix format
     print(i)
             
+#part-b
+
+def transpose(l,n):
+    L=[]
+    for i in range(n):
+        L.append([])
+
+    for i in l:
+        a=0
+        for j in i:
+            L[a].append(j)
+            a+=1
+
+    return L
+
+
+l=eval(input("Enter a 2d list: "))
+n=int(input("Enter the size of the 2d list: "))
+transposed_matrix=(transpose(l,n))
+
+if transposed_matrix==l:
+    print("The matrix is symmetric")
+
+elif transposed_matrix==-l:
+    print("The matrix is skew-symmetric")
+
+else:
+    print("The matrix is neither symmetric nor skew-symmetric")
 
--- test.py
+++ test.py
@@ -1,3 +1,29 @@
-print('hello world')
-ask=input('what is your name?')
-print('hello',ask)
+def transpose(l,n):
+    L=[]
+    for i in range(n):
+        L.append([])
+
+    for i in l:
+        a=0
+        for j in i:
+            L[a].append(j)
+            a+=1
+
+    return L
+
+def checking_skew(matrix):
+    
+
+
+l=eval(input("Enter a 2d list: "))
+n=int(input("Enter the size of the 2d list: "))
+transposed_matrix=(transpose(l,n))
+
+if transposed_matrix==l:
+    print("The matrix is symmetric")
+
+elif transposed_matrix==-:
+    print("The matrix is skew-symmetric")
+
+else:
+    print("The matrix is neither symmetric nor skew-symmetric")
