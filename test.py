13f8f12368e983aa87134fa964a13b3bc05897a4
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2023-09-10 16:41:27 +0530

    all done except bonus 1 and little fix required

--- test.py
+++ test.py
@@ -1,32 +1,41 @@
-#part-a
+def polynomial(degree):
+    l=[]
+    for i in range(degree+1):
+        l.append([0,0])
+    for j in range(degree+1):
+        l[j][0]=j
+        l[j][1]=int(input(f'Enter the coefficient of x^{j}: '))
 
-def create_2d_matrix(rows,cols):
-    L=[]
-    for i in range (1,rows+1):
-        L1=[]
-        for j in range (1,cols+1):
-            k=eval(input(f"Enter a element for position{i,j}: "))
-            L1.append(k)
+    return l
+
+
+def addi_(f1,f2):
+    l=[]
+    m=max(len(f1),len(f2))
+    n=min(len(f1),len(f2))
+    for i in range(m):
+        l.append([0,0])
         
-        L.append(L1)
-    return L
+    for j in range(n):
+        if f1[j][0]==f2[j][0]:
+            l[j][0]=f1[j][0]
+            l[j][1]=f1[j][1]+f2[j][1]
+        
+        else:
+            if f1[j][0]>f2[j][0]:
+                l[j][0]=f1[j][0]
+                l[j][1]=f1[j][1]
+            else:
+                l[j][0]=f2[j][0]
+                l[j][1]=f2[j][1]
 
-rows=int(input("Enter the number of rows: "))
-cols=int(input("Enter the number of columns: "))
-l=(create_2d_matrix(rows,cols))    #this creates a 2d list and stores it in a variable l
+    print(l)
+        
+
+f1=polynomial(int(input('degree for polynomial 1 ')))
+f2=polynomial(int(input('degree for polynomial 2 ')))
+addi_(f1,f2)
 
 
-for i in l:             #this prints the 2d list in matrix format
-    print(i)
 
-k=eval(input("Enter a number to check if it is present in the matrix: "))
-found=False
-for i in range(1,len(l)+1):
-    for j in range(1,len(l[0])+1):
-        if k==l[i-1][j-1]:
-            print(f"{k} is present in the matrix at position {i,j}")
-            found=True
 
-if not found:
-    print(f"{k} is not present in the matrix")
-    
