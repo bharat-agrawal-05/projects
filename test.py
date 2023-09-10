92f340c17cad23be833d400353fca06b17a093ca
Bharat Agrawal <141904232+bharatagrawal12004@users.noreply.github.com>
2023-09-10 10:40:40 +0530

    q4 done

--- test.py
+++ test.py
@@ -1,29 +1,32 @@
-def transpose(l,n):
+#part-a
+
+def create_2d_matrix(rows,cols):
     L=[]
-    for i in range(n):
-        L.append([])
-
-    for i in l:
-        a=0
-        for j in i:
-            L[a].append(j)
-            a+=1
-
+    for i in range (1,rows+1):
+        L1=[]
+        for j in range (1,cols+1):
+            k=eval(input(f"Enter a element for position{i,j}: "))
+            L1.append(k)
+        
+        L.append(L1)
     return L
 
-def checking_skew(matrix):
+rows=int(input("Enter the number of rows: "))
+cols=int(input("Enter the number of columns: "))
+l=(create_2d_matrix(rows,cols))    #this creates a 2d list and stores it in a variable l
+
+
+for i in l:             #this prints the 2d list in matrix format
+    print(i)
+
+k=eval(input("Enter a number to check if it is present in the matrix: "))
+found=False
+for i in range(1,len(l)+1):
+    for j in range(1,len(l[0])+1):
+        if k==l[i-1][j-1]:
+            print(f"{k} is present in the matrix at position {i,j}")
+            found=True
+
+if not found:
+    print(f"{k} is not present in the matrix")
     
-
-
-l=eval(input("Enter a 2d list: "))
-n=int(input("Enter the size of the 2d list: "))
-transposed_matrix=(transpose(l,n))
-
-if transposed_matrix==l:
-    print("The matrix is symmetric")
-
-elif transposed_matrix==-:
-    print("The matrix is skew-symmetric")
-
-else:
-    print("The matrix is neither symmetric nor skew-symmetric")
