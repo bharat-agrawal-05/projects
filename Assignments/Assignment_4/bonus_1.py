54a4972e8d5ab6706f33cc0fa8eb2bacab7fefb0
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2023-09-14 16:25:03 +0530

    bonus 1 rational roots code added

--- Assignments/Assignment_4/bonus_1.py
+++ Assignments/Assignment_4/bonus_1.py
@@ -1,4 +1,5 @@
 #method-1
+#the following code finds all the roots of a polynomial
 
 from numpy import roots as r 
 n=int(input("Enter the degree of polynomial"))
@@ -12,17 +13,23 @@ for i in l:
     
 
 #method-2
+#newton-raphson method 
+#only for rational roots
+
+
 import unicodeit
 print('To find the roots of a polynomial . . .') # Function of the Program
 
 f = []
 deg = int(input('Enter the highest degree of the polynomial : '))
 print('Enter coefficients from the highest degree to the constant . . .')
-for i in range(deg + 1) : f.append(float(input(unicodeit.replace(f'Enter a coefficient for x^{deg - i}: '))))
+for i in range(deg + 1) :
+    f.append(float(input(unicodeit.replace(f'Enter a coefficient for x^{deg - i}: '))))
 
 def check(val) : 
     result = 0
-    for i in range(len(f)) : result = result + f[i] * val ** (deg - i)
+    for i in range(len(f)) :
+        result = result + f[i] * val ** (deg - i)
     return result
 roots = []
 div = abs(int(f[deg] / f[0]))
@@ -30,5 +37,6 @@ for i in range(-1 * div, div + 1) :
     if check(i) == 0 : roots.append(i)
 roots = list(set(roots))
 print('The integral root(s) of the polynomial is/are . . .')
-for i in range(len(roots)) : print(roots[i], end = ', ')
+for i in range(len(roots)) :
+    print(roots[i], end = ', ')
 print('\b\b ')
