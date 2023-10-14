573362cd2b781cf6bf463354486bc5272e87dd25
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2023-10-14 23:03:41 +0530

    problem2 done

--- Assignments/Assignment_6/problem2.py
+++ Assignments/Assignment_6/problem2.py
@@ -1,20 +1,28 @@
-import csv
 
 myDict=[]
-with open('problem2input','r') as f:
-    d=f.read()
-    l=d.split()
+temp={}
+with open ('problem2Input','r') as f:
+    lines=f.readlines()
+    for line in lines:
+        for word in line.split():
+            if word in temp:
+                temp[word]+=1
+            else:
+                temp[word]=1
+                
+for i in list(temp.keys()):
+    myDict.append({'word':i,'frequency':temp[i]})
 
-    s=set(l)
-    for i in s:
-        myDict.append({'word':i,'frequency':l.count(i)})
+sorted_myDict=sorted(myDict,key=lambda x:x['frequency'],reverse=True)
 
-    myDict.sort(key=lambda x:x['frequency'],reverse=True)
-
-
+import csv
 with open('problem2Output.csv', 'w') as csvop: 
     # creating dictionary writer object
     writerObj = csv.DictWriter(csvop, fieldnames = ['word', 'frequency']) 
+
 	# write fieldnames
     writerObj.writeheader()
-    writerObj.writerows(myDict)
+    writerObj.writerows(sorted_myDict)
+
+
+
