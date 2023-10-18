e820023a20e4e7b46ae0c78d1e8c24219d1eca70
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2023-10-18 13:40:01 +0530

    problem2 fixes

--- Assignments/Assignment_6/problem2.py
+++ Assignments/Assignment_6/problem2.py
@@ -8,17 +8,16 @@ if len(sys.argv)==1:
     
 try:
     #the following is the code for reading the filepaths from command line arguments and storing the words and their frequencies in a dictionary
-    for i in range(1,len(sys.argv)):
-        filepath=sys.argv[i]
-        with open (filepath,'r') as f:
-            lines=f.readlines()
-            for line in lines:
-                for word in line.split():
-                    word = word.lower()
-                    if word in temp:
-                        temp[word]+=1
-                    else:
-                        temp[word]=1
+    filepath=sys.argv[1]
+    with open (filepath,'r') as f:
+        lines=f.readlines()
+        for line in lines:
+            for word in line.split():
+                word = word.lower()
+                if word in temp:
+                    temp[word]+=1
+                else:
+                    temp[word]=1
 
 except FileNotFoundError or Exception:
     print("Invalid path")
@@ -27,10 +26,8 @@ except FileNotFoundError or Exception:
 for i in list(temp.keys()):
     myDict.append({'word':i,'frequency':temp[i]})       #storing the words and their frequencies in a list of dictionaries
 
-def freq(item):
-    return item['frequency']
 
-sorted_myDict=sorted(myDict,key=freq,reverse=True)      #the Key is the function that is applied to each element of the iterable (list) and the return value of the function is used for sorting.
+sorted_myDict=sorted(myDict,key=lambda x:x['frequency'],reverse=True)      #the Key is the function that is applied to each element of the iterable (list) and the return value of the function is used for sorting.
 
 import csv
 with open('problem2Output.csv', 'w') as csvop: 
