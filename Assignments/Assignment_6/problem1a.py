3329ceaeb10028a743771a48bec05181d5b243be
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2023-10-20 22:40:37 +0530

    problem1a

--- Assignments/Assignment_6/problem1a.py
+++ Assignments/Assignment_6/problem1a.py
@@ -8,39 +8,47 @@ def is_int(n):
     except ValueError:
         return False
     
-for i in range(1,len(sys.argv)):
-    filepath=sys.argv[i]
-    with open(filepath,'r') as f:
-            l=f.readlines()
-            x=l[0].split()
-            y=l[1].split()
-            x1=[]
-            y1=[]
-            for i in range(1,len(x)):
-                if is_int(x[i]):
-                    x1.append(int(x[i]))
+if len(sys.argv)==1:        #checks if the filepath is given as command line argument or not
+    print("No filepaths given, Please provide filepaths as command line arguments")
+    exit()
+
+try:
+    for i in range(1,len(sys.argv)):
+        filepath=sys.argv[i]
+        with open(filepath,'r') as f:
+                l=f.readlines()
+                x=l[0].split()
+                y=l[1].split()
+                x1=[]
+                y1=[]
+                for i in range(1,len(x)):
+                    if is_int(x[i]):
+                        x1.append(int(x[i]))
+                    
+                for i in range(1,len(y)):
+                    if is_int(y[i]):
+                        y1.append(int(y[i]))
+
+                x1=np.array(x1)
+                y1=np.array(y1)
+                xmean=np.mean(x1)
+                ymean=np.mean(y1)
+                x=x1-xmean
+                y=y1-ymean
+                cos_theta_num=np.sum(x*y)        #dot product of x and y
+                cos_theta_den=np.sqrt(np.sum(np.square(x))*np.sum(np.square(y)))   #product of magnitude of x and y
+                cos_theta=cos_theta_num/cos_theta_den
+                print(f'{cos_theta} is the cosine of the angle between x and y')
+
+                #finding r using the formula
+                rnum=(len(x1)*np.sum(x1*y1))-(np.sum(x1)*np.sum(y1))
+                rden=np.sqrt((len(x1)*np.sum(np.square(x1))-np.square(np.sum(x1)))*(len(y1)*np.sum(np.square(y1))-np.square(np.sum(y1))))
+                r=rnum/rden
+                print(f'{r} is the correlation coefficient between x and y')
                 
-            for i in range(1,len(y)):
-                if is_int(y[i]):
-                    y1.append(int(y[i]))
+except FileNotFoundError or Exception:
+    print(f"File with Filepath - '{filepath}' , not found")
 
-            x1=np.array(x1)
-            y1=np.array(y1)
-            xmean=np.mean(x1)
-            ymean=np.mean(y1)
-            x=x1-xmean
-            y=y1-ymean
-            cos_theta_num=np.sum(x*y)        #dot product of x and y
-            cos_theta_den=np.sqrt(np.sum(np.square(x))*np.sum(np.square(y)))   #product of magnitude of x and y
-            cos_theta=cos_theta_num/cos_theta_den
-            print(cos_theta)
-
-            #finding r using the formula
-            rnum=(len(x1)*np.sum(x1*y1))-(np.sum(x1)*np.sum(y1))
-            rden=np.sqrt((len(x1)*np.sum(np.square(x1))-np.square(np.sum(x1)))*(len(y1)*np.sum(np.square(y1))-np.square(np.sum(y1))))
-            r=rnum/rden
-            print(r)
-            
 
             
 
