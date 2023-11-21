e31c8f05f718fcecc258d2c99e93a3b482a0c89a
Bharat Agrawal <141904232+bharat-agrawal-05@users.noreply.github.com>
2023-11-21 16:57:11 +0530

    done

--- Assignments/Assignment_9/problem1.py
+++ Assignments/Assignment_9/problem1.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import csv
 import sys
 
 #part-a
-df=pd.read_csv('Cars93.csv')
+df=pd.read_csv('data/Cars93.csv')
 a=list(df['Model'].sort_values(ascending=True))
 df=pd.DataFrame(a,columns=['Model Sorted'])
 df.index=df.index+1
@@ -11,18 +10,11 @@ df.to_csv('problem1a.csv')
 
 #part-b
 
-df=pd.read_csv('Cars93.csv')
-types=df['Type'].unique()
-with open('problem1b.csv','w') as f:
-    w=csv.writer(f)
-    w.writerow(df.columns)
-
+df=pd.read_csv('data/Cars93.csv')
+types=list(df['Type'].unique())
 for i in types:
     data=df.groupby('Type').get_group(i)
-    required_data=pd.DataFrame(data[data['Max.Price']==data['Max.Price'].max()])
-    with open('problem1b.csv','a') as f:
-        writer=csv.writer(f)
-        writer.writerows(required_data.values)
+    print(data[data['Max.Price']==data['Max.Price'].max()])
     
 #part-c
 
@@ -30,14 +22,17 @@ if len(sys.argv)!=2:
     print('Enter Name of the Manufacturer as command line argument')
     exit()
 
-input=sys.argv[1]
-df=pd.read_csv('Cars93.csv')
-required_data=df[df['Manufacturer']==input]['Model']
-print(required_data)
+try:
+    input=sys.argv[1]
+    df=pd.read_csv('data/Cars93.csv')
+    required_data=df[df['Manufacturer']==input]['Model']
+    print(required_data)
+except:
+    print('Invalid Input')
+    exit()
 
 #part-d
-df=pd.read_csv('Cars93.csv')
+df=pd.read_csv('data/Cars93.csv')
 required_data=df.groupby('Manufacturer').size().sort_values(ascending=False)
-header=['Count']
-df=pd.DataFrame(required_data,columns=header)
+df=pd.DataFrame({'Manufacturer':required_data.index,'Count':required_data.values},index=[i for i in range(1,len(required_data)+1)])
 df.to_csv('problem1d.csv')
